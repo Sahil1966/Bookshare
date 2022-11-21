0ed372eb5f963e7ff523c92e7d37555e818974e5
Sahil Patel <sahil.patel.csai.20@ggits.net>
2022-11-21 18:42:53 +0530

    Code pushed

---
+++ routes/books.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getBooks,
+  createBookAd,
+  addToWishList,
+  updateIsSold,
+  deleteaBook,
+  editBook,
+} = require("../controllers/books");
+const auth = require("../middleware/auth");
+
+router.get("/all", getBooks);
+router.route("/books").get(getBooks)
+router.post("/add", auth, createBookAd);
+router.patch("/:id/addWishList", auth, addToWishList);
+router.patch("/:id/sold", auth, updateIsSold);
+router.delete("/:id", auth, deleteaBook);
+router.patch("/:id", auth, editBook); 
+
+module.exports = router;
